9114ea62517f71c81bd1ae06dad0515ab3ac81e8
Nichole <n.r.griffith@gmail.com>
2018-01-28 10:25:54 -0600

    added problem 3

--- homework2/problem1.m
+++ homework2/problem1.m
@@ -8,26 +8,25 @@
 clear all
 clc
 
-syms t s F
+syms s F
 
 % Constants defined by problem statement
-k = 1;
-B = 1;
-M_1 = 1;
-M_2 = 1;
+k = 1;        % N/m
+B = 1;        % N*s/m
+M_1 = 1;      % kg
+M_2 = 1;      % kg
 
 A = [k+B*s+M_1*s^2 -k-B*s; -k-B*s k+B*s+M_2*s^2]; % Coefficient Matrix
 Ax_1 = [F -k-B*s; 0 k+B*s+M_2*s^2];               % x_1 Matrix
 Ax_2 = [k+B*s+M_1*s^2 F; -k-B*s 0];               % x_2 Matrix
 
 X_1 = det(Ax_1)/det(A);
-disp('X_1(s) = ')
-pretty(X_1)
+%disp('X_1(s) = ')
+%pretty(X_1)
 
 X_2 = det(Ax_2)/det(A);
-disp('X_2(s) = ')
-pretty(X_2)
+%disp('X_2(s) = ')
+%pretty(X_2)
 
 disp('Transfer Function G(s)=')
-G = simplify(X_2/F);
-pretty(G)
+pretty(simplify(X_2/F))
--- homework2/problem2.m
+++ homework2/problem2.m
@@ -8,16 +8,17 @@
 clear all
 clc
 
-syms t s F
+syms s F
 
-B_1 = 2;
-B_2 = 2;
-B_3 = 2;
-M_1 = 4;
-M_2 = 4;
-M_3 = 4;
-K_1 = 6;
-K_2 = 6;
+% Constants defined by problem statement
+B_1 = 2;        % N*s/m
+B_2 = 2;        % N*s/m
+B_3 = 2;        % N*s/m
+M_1 = 4;        % kg
+M_2 = 4;        % kg
+M_3 = 4;        % kg
+K_1 = 6;        % N/m
+K_2 = 6;        % N/m
 
 A = [K_1+M_1*s^2+B_2*s -B_2*s 0; -B_2*s B_1*s+B_2*s+M_2*s^2+K_2 -K_2; 0 -K_2 K_2+M_3*s^2+B_3*s];
 Ax_1 = [0 -B_2*s 0; F B_1*s+B_2*s+M_2*s^2+K_2 -K_2; 0 -K_2 K_2+M_3*s^2+B_3*s];
@@ -28,4 +29,5 @@ X_1 = det(Ax_1)/det(A);
 X_2 = det(Ax_2)/det(A);
 X_3 = det(Ax_3)/det(A);
 
+disp('Transfer Function G(s)=')
 pretty(simplify(X_3/F))
