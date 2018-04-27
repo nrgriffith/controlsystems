edd3990e81d9062d4d8b6c9d512f91db12184a3b
Griffith <ngriffith@uiowa.edu>
2018-04-27 12:18:14 -0500

    finished homework11

--- homework11/problem3.m
+++ homework11/problem3.m
@@ -1,5 +1,24 @@
 s = tf('s');
 G = 2000000000/(s*(s+100)*(s+500)*(s+800));
+pm = 57;
+wg = 45;
 
+zeta = pm/100;
+wn = wg/(sqrt(sqrt(4*zeta^4 + 1) -2*zeta^2));
+
+figure(1)
 bode(G)
 grid
+
+G_ClosedLoop = feedback(G,1);
+
+G_approx = wn^2/(s*(s+2*zeta*wn));
+G_approx_ClosedLoop = feedback(G_approx,1);
+
+figure(2)
+subplot(1,2,1)
+step(G_ClosedLoop)
+title('Step Response for CL G(s)')
+subplot(1,2,2)
+step(G_approx_ClosedLoop)
+title('Step Response for 2nd Order Approx. CL G(s)')
