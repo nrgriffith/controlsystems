7ce9950bf8f474ce4f76878e0cdd42a66db96272
Nichole <n.r.griffith@gmail.com>
2018-04-30 20:33:17 -0500

    lab3 graphs

--- lab3/postlabplots.m
+++ lab3/postlabplots.m
@@ -1,28 +1,41 @@
-K = 23;
+%%%%%% Put the K value here %%%%%%
+K = 98;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Load file
 file = strcat('data/k',num2str(K));
 file = strcat(file,'.mat');
-
 load(file)
 
-anglex = data_ang(:,1);
-vmx = data_Vm(:,1);
-xcx = data_xc(:,1);
+% Offset x-axis (so it starts at 0 seconds)
+offset = data_ang(1,1);
+anglex = data_ang(:,1) - offset;
+xcx = data_xc(:,1) - offset;
+vmx = data_Vm(:,1) - offset;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Generate simulation data
 constants
 open_loop
 closed_loop
-
-% Generate Square Wave
 [u,t]=gensig('square',10,20,0.1);
-
-% Simulate
 [simy, simt] = lsim(T,u,t);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Plots
 figure(1)
 plot(anglex,data_ang(:,2)),ylabel('Angle (Radians)'),xlabel('Time (seconds)'),title(['Angle Variation (Gain K = ' num2str(K) ')'])
 
 figure(2)
 plot(simt,simy(:,2),'--k')
+
+figure(3)
+plot(xcx,data_xc(:,2)),ylabel('Position (millimeters)'),xlabel('Time (seconds)'),title(['Cart Position Variation (Gain K = ' num2str(K) ')'])
+
+figure(4)
+plot(simt,simy(:,1),'--k')
+
+figure(5)
+plot(vmx,data_Vm(:,2)),ylabel('Motor Voltage (volts)'),xlabel('Time (seconds)'),title(['Motor Voltage vs. Time (Gain K = ' num2str(K) ')'])
